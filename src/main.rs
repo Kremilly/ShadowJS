5a310160dec637243736a6acd32fb7d488072078
Kremilly <contact@kremilly.com>
2024-12-27 13:22:26 -0300

    feat: add command-line argument support for input and output files

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 mod core;
 mod utils;
 
+use std::{
+    env, 
+    process
+};
+
 use crate::core::engine::Engine;
 
 fn main() {
-    let input_file = "input.js";
-    let output_file = "output.min.js";
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Incorrect usage. Example: shadowjs <input.js> <output.js>");
+        process::exit(1);
+    }
+
+    let input_file = &args[1];
+    let output_file = &args[2];
 
     let _ = Engine::new(input_file, output_file).run();
 }
